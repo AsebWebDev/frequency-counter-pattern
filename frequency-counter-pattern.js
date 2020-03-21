4e4990b311f3646aa0f545fa46028562e77096c3
André Sebastian <a.sebastian@posteo.de>
2020-03-21 18:26:49 +0100

    solution

--- frequency-counter-pattern.js
+++ frequency-counter-pattern.js
@@ -1,8 +1,23 @@
-function validAnagram(str) {
-    let obj = {};
-    for (char in str) {
-        console.log(str[char])
+function validAnagram(str1, str2) {
+    // not an anagram, when different length
+    if ( str1.length !== str2.length ) return false;
+
+    const obj = {};
+    for (char in str1) {
+        let letter = str1[char];
+        obj[letter] ? obj[letter] += 1 : obj[letter] = 1;
     }
+    console.log(obj)
+
+
+    for (char in str2) {
+        let letter = str2[char];
+        if (!obj[letter]) return false 
+        else obj[letter] -= 1
+    }
+
+    console.log(obj)
+    return true
 }
 
-validAnagram("tes")
+console.log(validAnagram("tes", "set"))
